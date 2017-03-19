c06228857bd0e1b2786d7f5cfda4e2b7538830d1
TN1ck <tomwanick@gmail.com>
2017-03-19 17:54:03 +0100

    feat: adds some generated sudokus

---
+++ src/sudokus/index.ts
@@ -0,0 +1,11 @@
+import easySudokus from './easy';
+import mediumSudokus from './medium';
+import hardSudokus from './hard';
+import evilSudokus from './evil';
+
+export default {
+    easy: easySudokus,
+    medium: mediumSudokus,
+    hard: hardSudokus,
+    evil: evilSudokus
+};
