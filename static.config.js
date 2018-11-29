81750a97948d5c9b9e6b1cda8836f8617217806d
Tom Nick <tomwanick@gmail.com>
2018-11-30 00:14:39 +0100

    chore: change some metatags

--- static.config.js
+++ static.config.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ServerStyleSheet } from 'styled-components'
+import {ServerStyleSheet} from "styled-components";
 import path from "path";
 import fs from "fs";
 import showdown from "showdown";
@@ -8,14 +8,14 @@ import ExtractTextPlugin from "extract-text-webpack-plugin";
 const converter = new showdown.Converter();
 
 const URL = "https://practical-bartik-1e6a2d.netlify.com";
-const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
 function getPrivacyHtml() {
   return new Promise(resolve => {
-    fs.readFile(path.join(__dirname, 'src/privacy.md'), 'utf8', (err, text) => {
+    fs.readFile(path.join(__dirname, "src/privacy.md"), "utf8", (err, text) => {
       const html = converter.makeHtml(text);
       resolve(html);
-    })
+    });
   });
 }
 
@@ -30,19 +30,19 @@ export default {
     return [
       {
         path: "/",
-        component: 'src/components/pages/Game'
+        component: "src/components/pages/Game",
       },
       {
         path: "/playground",
-        component: "src/components/pages/Playground"
+        component: "src/components/pages/Playground",
       },
       {
         path: "/about",
-        component: 'src/components/pages/About'
+        component: "src/components/pages/About",
       },
       {
         path: "404",
-        component: 'src/components/pages/404'
+        component: "src/components/pages/404",
       },
     ];
   },
@@ -52,34 +52,37 @@ export default {
     meta.styleTags = sheet.getStyleElement();
     return html;
   },
-  Document: ({ Html, Head, Body, children, siteData, renderMeta }) => (
+  Document: ({Html, Head, Body, children, siteData, renderMeta}) => (
     <Html lang="en-US">
       <Head>
         <meta charSet="UTF-8" />
-        <title>Awesome Newsletter</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, shrink-to-fit=no, user-scalable=no" />
+        <title>Super Sudoku</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1.0, shrink-to-fit=no, user-scalable=no"
+        />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
-        <meta name="description" content="Create the most amazing newsletters"/>
-        <meta name="keywords" content="Newsletter,Newsletter Creator,Mails,E-mails"/>
-        <meta property="og:title" content="awesome-newsletters.io"/>
-        <meta property="og:site_name" content="awesome-newsletters.io"/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:url" content={URL}/>
-        <meta property="og:description" content="Create awesome newsletters"/>
-        <meta property="og:image" content={`${URL}/logo-512.png`}/>
-        <meta property="og:image:type" content="image/png"/>
-        <meta property="og:image:width" content="512"/>
-        <meta property="og:image:height" content="512"/>
+        <meta name="description" content="Play sudoku games from easy to evil" />
+        <meta name="keywords" content="Sudoku,Sudoku App,Game,Sudoku Generate" />
+        <meta property="og:title" content="Super Sudoku" />
+        <meta property="og:site_name" content="Super Sudoku" />
+        <meta property="og:type" content="website" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={URL} />
+        <meta property="og:description" content="Play sudoku games from easy to evil" />
+        <meta property="og:image" content={`${URL}/logo-512.png`} />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="512" />
+        <meta property="og:image:height" content="512" />
 
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#999999" />
-        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet" />
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="theme-color" content="#ffffff" />
         {renderMeta.styleTags}
@@ -87,14 +90,14 @@ export default {
       <Body>{children}</Body>
     </Html>
   ),
-  webpack: (config, { defaultLoaders, stage }) => {
+  webpack: (config, {defaultLoaders, stage}) => {
     // Add .ts and .tsx extension to resolver
     config.resolve.extensions.push(".ts", ".tsx");
 
     const tsconfig = require("./tsconfig.json");
 
     config.resolve.alias = {
-      src: path.join(__dirname, 'src'),
+      src: path.join(__dirname, "src"),
     };
 
     config.module.rules = [
@@ -119,16 +122,14 @@ export default {
           defaultLoaders.fileLoader,
           {
             test: /\.(json)$/,
-            include: path.join(__dirname, 'src/assets'),
-            use: 'file-loader',
-          }
+            include: path.join(__dirname, "src/assets"),
+            use: "file-loader",
+          },
         ],
       },
     ];
 
-    config.plugins.push(
-      new ExtractTextPlugin("styles.css"),
-    );
+    config.plugins.push(new ExtractTextPlugin("styles.css"));
 
     return config;
   },
