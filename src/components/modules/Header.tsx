5b7b745f9bf2c7a88d7dc9775faf960fde64de02
Tom Nick <tomwanick@gmail.com>
2022-09-03 20:14:35 +0200

    feat: add by tn1ck link

--- src/components/modules/Header.tsx
+++ src/components/modules/Header.tsx
@@ -5,8 +5,11 @@ import {connect} from "react-redux";
 
 const Header = ({}) => {
   return (
-    <div className="flex p-4 bg-gray-900 text-white">
+    <div className="flex justify-between bg-gray-900 p-4 text-white">
       <div>{"Super Sudoku"}</div>
+      <a href="https://tn1ck.com" target="_blank" className="text-gray-500 hover:underline">
+        {"By tn1ck.com"}
+      </a>
     </div>
   );
 };
